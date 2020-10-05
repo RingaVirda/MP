1f64781cd72d2fd08dfc31e02100f9d4530dee85
Ringa | Virda <ringanaro@gmail.com>
2020-10-05 20:18:12 +0300

    It finally works

---
+++ AndroidTest/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace AndroidTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", 
+        MainLauncher = true, Description = "@string/org_name",
+        Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
